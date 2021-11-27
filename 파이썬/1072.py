b47fb5fb466926ab4c0338e6bcb91ebfe2b572bc
leezzangmin <64303390+leezzangmin@users.noreply.github.com>
2021-11-27 18:49:22 +0900

    Add : Day 111_2

--- 파이썬/1072.py
+++ 파이썬/1072.py
@@ -1,5 +1,24 @@
-X,Y=map(int,input().split())
+import sys
+input=sys.stdin.readline
+X,Y=map(int, input().rsplit())
 
-Z=int((Y/X)*100)
-print(Z)
-#print(int(Z*100))
+Z = Y * 100 // X
+# (y + mid) * 100 // (x + mid)
+ans = sys.maxsize
+left,right=1,X
+
+while left<=right:
+    mid=(left+right)//2
+    temp=(Y+mid)*100//(X+mid)
+
+    if Z>=temp:
+        left=mid+1
+    else:
+        ans=mid
+        right=mid-1
+if ans==sys.maxsize:
+    print(-1)
+else:
+    print(ans)
+
+    
