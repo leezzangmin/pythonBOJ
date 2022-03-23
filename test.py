9f9cdcf73feb26414a92871c089973cef562851f
leezzangmin <ckdals12345678@gmail.com>
2022-03-23 21:30:26 +0900

    Add : Day 160

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print(0==False)
+from itertools import combinations
+op=["+","-","*",]
+op=[2,1,1,1]
+ops=""
+for i in range(4):
+    if i==0:
+        for i in range(op[i]):
+            ops+="+"
+    elif i==1:
+        for i in range(op[i]):
+            ops+="-"
+    elif i==2:
+        for i in range(op[i]):
+            ops+="*"
+    else:
+        for i in range(op[i]):
+            ops+="/"
+print(ops)
+c=combinations(ops,5)
+ans=0
+for i in c:
+    #print(i)
+    ans+=1
+print(ans)
--- 파이썬/2579_2.py
+++ 파이썬/2579_2.py
@@ -1,6 +1,12 @@
-import sys;input=sys.stdin.readline
-N=int(input())
-s=[0]*N
-dp=[0]*N
-for i in range(N):
-    s[i]=int(input())
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+stairs = [0] + [int(read()) for _ in range(n)] + [0]
+cache = [0] * (n+2)
+cache[1] = stairs[1]
+cache[2] = cache[1] + stairs[2]
+
+for i in range(3, n+1):
+    cache[i] = max(cache[i-2], cache[i-3] + stairs[i-1]) + stairs[i]
+print(cache[n])
