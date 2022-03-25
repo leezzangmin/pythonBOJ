2c5bd6802ca16acb659ccc966f9cd55a268791a3
leezzangmin <ckdals12345678@gmail.com>
2022-03-25 22:57:46 +0900

    Add : Day 161

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-from itertools import combinations
-op=["+","-","*",]
-op=[2,1,1,1]
-ops=""
-for i in range(4):
-    if i==0:
-        for i in range(op[i]):
-            ops+="+"
-    elif i==1:
-        for i in range(op[i]):
-            ops+="-"
-    elif i==2:
-        for i in range(op[i]):
-            ops+="*"
-    else:
-        for i in range(op[i]):
-            ops+="/"
-print(ops)
-c=combinations(ops,5)
-ans=0
-for i in c:
-    #print(i)
-    ans+=1
-print(ans)
+a=[]
+b='asdf'
+if a:
+    b=True
+else:
+    b=False
+print(b)
