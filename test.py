d07b34c23a063d5eb89035b28b28f2e93efcc9ce
leezzangmin <ckdals12345678@gmail.com>
2022-04-08 00:15:28 +0900

    Add : Day 167

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-N=5
-city=list([] for _ in range(N+1))
-print(city)
+import bisect
+
+l=[-3,-2,1,0,1,2,3,4,100,200,300,400]
+
+a=bisect.bisect_left(l,-3)
+b=bisect.bisect_right(l,-3)
+print(a)
+print(b)
