729e35b0d9479601f50d86faa931955380b3a635
leezzangmin <ckdals12345678@gmail.com>
2022-04-11 17:11:44 +0900

    Add : Day 168

--- 파이썬/2110.py
+++ 파이썬/2110.py
@@ -12,16 +12,13 @@ right=house[-1]-house[0]
 answer=0
 while left<=right:
     mid=(left+right)//2
-    print(mid,'mm')
     cur=house[0]
     D=C-1
     for i in range(1,N):
         print(house[i]-cur,'--')
         if house[i]-cur>=mid:
             cur=house[i]
-            print(cur,'asdf')
             D-=1
-    print(D,'DDD')
     if D<=0:
         answer=mid
         left=mid+1
