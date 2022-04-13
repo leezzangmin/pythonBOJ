275add48688abc30bdba3b0e9d32e027ac5db05c
leezzangmin <ckdals12345678@gmail.com>
2022-04-13 20:08:07 +0900

    Add : Day 170

--- 프로그래머스/모음사전.py
+++ 프로그래머스/모음사전.py
@@ -1,1 +1,22 @@
-# https://programmers.co.kr/learn/courses/30/lessons/84512?language=java
+def solution(word):
+    answer = 0
+ 
+    word_list = []
+    words = "AEIOU"
+ 
+    # 모든 단어조합의 경우를 찾는 함수
+    def all_word(cnt, w):
+        if cnt == 5:
+            return
+ 
+        for i in range(len(words)):
+            word_list.append(w + words[i])
+            all_word(cnt + 1, w + words[i])
+ 
+ 
+    all_word(0, "")
+ 
+ 
+    return word_list.index(word) + 1
+#완전탐색 : 5 X 6 X 6 X 6 X 6
+#https://foameraserblue.tistory.com/187
