d907cb3ede1368324c2d64bbcde8d3c298f913ef
leezzangmin <ckdals12345678@gmail.com>
2022-04-16 16:31:27 +0900

    Add : Day 171

---
+++ 파이썬/5582.py
@@ -0,0 +1,13 @@
+A=input()
+B=input()
+
+arr = [[0]*(len(B)+1) for _ in range(len(A)+1)]
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
+            arr[i][j] = arr[i-1][j-1] + 1
+ 
+ans=0
+for i in arr:
+    ans=max(max(i),ans)
+print(ans)
