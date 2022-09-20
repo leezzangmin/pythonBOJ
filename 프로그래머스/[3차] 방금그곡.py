f65adb8cc8d3b1f5eaa21c28141e046f098d5d33
leezzangmin <ckdals12345678@gmail.com>
2022-09-21 00:52:37 +0900

    Add : Day 212

---
+++ 프로그래머스/[3차] 방금그곡.py
@@ -0,0 +1,54 @@
+def time_diff(start,end):
+    # if int(end[0:2])==0:
+    #     hour=24-int(start[0:2])
+    # else:
+
+    hour=int(end[0:2])-int(start[0:2])
+    minute=int(end[3:5])-int(start[3:5])
+    if minute<0:
+        hour-=1
+        minute=abs(minute)
+    return 60*hour+minute
+    # hour = 1 * (int(end.split(':')[0]) - int(start.split(':')[0]))
+    # if hour == 0: 
+    #     total = int(end.split(':')[1]) - int(start.split(':')[1])
+    # else: 
+    #     total = 60 * hour + int(end.split(':')[1]) - int(start.split(':')[1])
+    # return total
+
+def encode(chord):
+    return chord.replace('C#','Q').replace('D#','W').replace('F#','R').replace('G#','P').replace('A#','Y')
+    
+def music_duration(chord):
+    return len(chord)
+
+def solution(m, musicinfos):
+    answer = []
+    m=encode(m)
+    for idx,music in enumerate(musicinfos):
+        music=music.split(',')
+        print(music,'mmmm')
+        time=time_diff(music[0],music[1])
+        music[-1]=encode(music[-1])
+        chords=''
+        if time<music_duration(music[-1]):
+            chords=music[-1][0:time]
+        else:
+            a = time // music_duration(music[-1])
+            b = time % music_duration(music[-1])
+            chords+=music[-1]*a + music[-1][0:b]
+        print(chords,'chord')
+        if m in chords:
+            answer.append([-time,idx,music[2]])
+    answer.sort(key=lambda x:(x[0],x[1]))
+    print(answer,'answer')
+
+    if answer:
+        return answer[0][2]
+    else:
+        return '(None)'
+
+
+
+
+print(solution("A",	["12:00,12:06,HELLO,CDEFGAB", "13:00,13:07,WORLD,ABCDEF"]))
